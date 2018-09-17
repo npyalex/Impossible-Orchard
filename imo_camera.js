69f838484e67f692d9c22308cf72886748e19fa4
npyalex <np.alexander@gmail.com>
2018-09-17 14:31:26 -0400

    Post-Presentation Push
    Including tweaks made on the spot to make the project a little more presentable.

--- imo_camera.js
+++ imo_camera.js
@@ -1,5 +1,5 @@
 var capture;
-
+//set up the non-tracking camera
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO);
@@ -7,10 +7,11 @@ function setup() {
   capture.hide();
   background(255, 0, 200);
 }
-
+// camera resizes on window-resize
 function windowResized() {
     resizeCanvas(innerWidth, innerHeight);
 }
+// draw the camera image
 function draw() {
   background(255);
   image(capture, 0, 0, windowWidth, windowHeight);
